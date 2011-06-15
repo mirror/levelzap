3c256208916b5849ee17a501e78a3c272ab6d248
clechasseur_cp <SND\clechasseur_cp>
2011-06-15 03:12:40 -0700

    + Tag for version 1.0

---
+++ tags/1.0/LevelZap/src/dllmain.cpp
@@ -0,0 +1,39 @@
+// dllmain.cpp
+// (c) 2011, Charles Lechasseur
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "stdafx.h"
+#include "resource.h"
+#include "LevelZap_i.h"
+#include "dllmain.h"
+#include "xdlldata.h"
+
+CLevelZapModule _AtlModule;
+
+// DLL Entry Point
+extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+#ifdef _MERGE_PROXYSTUB
+	if (!PrxDllMain(hInstance, dwReason, lpReserved))
+		return FALSE;
+#endif
+	hInstance;
+	return _AtlModule.DllMain(dwReason, lpReserved); 
+}
