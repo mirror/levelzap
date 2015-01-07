77c8f7b7323b7522f5e017be1ec884736738b0bb
John Sebastian Peterson <john.s.peterson@live.com>
2015-01-07 22:52:04 +0100

    tabify
    because i dont want the productivity tools mixed state warning

--- LevelZap/src/LevelZapContextMenuExt.cpp
+++ LevelZap/src/LevelZapContextMenuExt.cpp
@@ -35,9 +35,9 @@
 // Constructor.
 //
 CLevelZapContextMenuExt::CLevelZapContextMenuExt()
-    : m_vFolders(),
-      m_FirstCmdId(),
-      m_ZapCmdId()
+	: m_vFolders(),
+	  m_FirstCmdId(),
+	  m_ZapCmdId()
 {
 }
 
@@ -53,46 +53,46 @@ CLevelZapContextMenuExt::CLevelZapContextMenuExt()
 // @return S_OK if successful, otherwise an error code.
 //
 STDMETHODIMP CLevelZapContextMenuExt::Initialize(
-    PCIDLIST_ABSOLUTE /*p_pFolderPIDL*/,
-    IDataObject *p_pDataObject,
-    HKEY /*p_hKeyFileClass*/)
+	PCIDLIST_ABSOLUTE /*p_pFolderPIDL*/,
+	IDataObject *p_pDataObject,
+	HKEY /*p_hKeyFileClass*/)
 {
-    HRESULT hRes = S_OK;
+	HRESULT hRes = S_OK;
 
-    try {
-        // Make sure we have a data object.
-        if (p_pDataObject != 0) {
-            // Extract HDROP from data object.
-            StStgMedium stgMedium;
-            FORMATETC formatEtc = {CF_HDROP, 0, DVASPECT_CONTENT, -1, TYMED_HGLOBAL};
-            if (SUCCEEDED(p_pDataObject->GetData(&formatEtc, &stgMedium))) {
-                // Get number of folders included in the selection.
-                UINT folderCount = ::DragQueryFileW(
-                    static_cast<HDROP>(stgMedium.Get().hGlobal), 0xFFFFFFFF, 0, 0);
-                if (folderCount > 0) {
-                    // Pre-allocate space in vector to store folders.
-                    m_vFolders.reserve(folderCount);
+	try {
+		// Make sure we have a data object.
+		if (p_pDataObject != 0) {
+			// Extract HDROP from data object.
+			StStgMedium stgMedium;
+			FORMATETC formatEtc = {CF_HDROP, 0, DVASPECT_CONTENT, -1, TYMED_HGLOBAL};
+			if (SUCCEEDED(p_pDataObject->GetData(&formatEtc, &stgMedium))) {
+				// Get number of folders included in the selection.
+				UINT folderCount = ::DragQueryFileW(
+					static_cast<HDROP>(stgMedium.Get().hGlobal), 0xFFFFFFFF, 0, 0);
+				if (folderCount > 0) {
+					// Pre-allocate space in vector to store folders.
+					m_vFolders.reserve(folderCount);
 
-                    // Get each file in turn.
-                    wchar_t buffer[MAX_PATH + 1];
-                    for(UINT i = 0; i < folderCount; ++i) {
-                        UINT copiedCount = ::DragQueryFileW(static_cast<HDROP>(stgMedium.Get().hGlobal),
-                            i, buffer, sizeof(buffer) / sizeof(wchar_t));
-                        m_vFolders.push_back(CString(buffer, copiedCount));
-                    }
-                } else {
-                    // It's difficult to display a menu item without files to act upon.
-                    hRes = E_FAIL;
-                }
-            }
-        } else {
-            hRes = E_POINTER;
-        }
-    } catch (...) {
-        hRes = E_UNEXPECTED;
-    }
+					// Get each file in turn.
+					wchar_t buffer[MAX_PATH + 1];
+					for(UINT i = 0; i < folderCount; ++i) {
+						UINT copiedCount = ::DragQueryFileW(static_cast<HDROP>(stgMedium.Get().hGlobal),
+							i, buffer, sizeof(buffer) / sizeof(wchar_t));
+						m_vFolders.push_back(CString(buffer, copiedCount));
+					}
+				} else {
+					// It's difficult to display a menu item without files to act upon.
+					hRes = E_FAIL;
+				}
+			}
+		} else {
+			hRes = E_POINTER;
+		}
+	} catch (...) {
+		hRes = E_UNEXPECTED;
+	}
 
-    return hRes;
+	return hRes;
 }
 
 //
@@ -110,47 +110,47 @@ STDMETHODIMP CLevelZapContextMenuExt::Initialize(
 //         used, plus one; otherwise, an error code.
 //
 STDMETHODIMP CLevelZapContextMenuExt::QueryContextMenu(
-    HMENU p_hMenu,
-    UINT p_Index,
-    UINT p_FirstCmdId,
-    UINT p_LastCmdId,
-    UINT p_Flags)
+	HMENU p_hMenu,
+	UINT p_Index,
+	UINT p_FirstCmdId,
+	UINT p_LastCmdId,
+	UINT p_Flags)
 {
-    HRESULT hRes = S_OK;
+	HRESULT hRes = S_OK;
 
-    try {
-        if (p_hMenu == 0) {
-            hRes = E_INVALIDARG;
-        } else {
-            // Do not add items if the default action is chosen or if we have no folders.
-            if (m_vFolders.empty() || (p_Flags & CMF_DEFAULTONLY) != 0) {
-                hRes = E_FAIL;
-            } else {
-                UINT cmdId = p_FirstCmdId;
-                UINT position = p_Index;
+	try {
+		if (p_hMenu == 0) {
+			hRes = E_INVALIDARG;
+		} else {
+			// Do not add items if the default action is chosen or if we have no folders.
+			if (m_vFolders.empty() || (p_Flags & CMF_DEFAULTONLY) != 0) {
+				hRes = E_FAIL;
+			} else {
+				UINT cmdId = p_FirstCmdId;
+				UINT position = p_Index;
 
-                // Insert "zap" menu item. We have only one so it's pretty easy.
-                CString zapMenuDesc(MAKEINTRESOURCE(IDS_ZAP_MENU_ITEM_DESCRIPTION));
-                if (::InsertMenu(p_hMenu, position, MF_STRING | MF_BYPOSITION, cmdId, zapMenuDesc)) {
-                    m_FirstCmdId = cmdId;
-                    m_ZapCmdId = cmdId;
-                    ++cmdId;
-                    ++position;
-                } else {
-                    hRes = E_FAIL;
-                }
+				// Insert "zap" menu item. We have only one so it's pretty easy.
+				CString zapMenuDesc(MAKEINTRESOURCE(IDS_ZAP_MENU_ITEM_DESCRIPTION));
+				if (::InsertMenu(p_hMenu, position, MF_STRING | MF_BYPOSITION, cmdId, zapMenuDesc)) {
+					m_FirstCmdId = cmdId;
+					m_ZapCmdId = cmdId;
+					++cmdId;
+					++position;
+				} else {
+					hRes = E_FAIL;
+				}
 
-                if (SUCCEEDED(hRes)) {
-                    // Strange return value requirement... see MSDN for details.
-                    hRes = MAKE_HRESULT(SEVERITY_SUCCESS, 0, cmdId - p_FirstCmdId + 1);
-                }
-            }
-        }
-    } catch (...) {
-        hRes = E_UNEXPECTED;
-    }
+				if (SUCCEEDED(hRes)) {
+					// Strange return value requirement... see MSDN for details.
+					hRes = MAKE_HRESULT(SEVERITY_SUCCESS, 0, cmdId - p_FirstCmdId + 1);
+				}
+			}
+		}
+	} catch (...) {
+		hRes = E_UNEXPECTED;
+	}
 
-    return hRes;
+	return hRes;
 }
 
 //
@@ -163,9 +163,9 @@ STDMETHODIMP CLevelZapContextMenuExt::QueryContextMenu(
 // @return S_OK if successful, otherwise an error code.
 //
 STDMETHODIMP CLevelZapContextMenuExt::InvokeCommand(
-    CMINVOKECOMMANDINFO* p_pCommandInfo)
+	CMINVOKECOMMANDINFO* p_pCommandInfo)
 {
-    HRESULT hRes = S_OK;
+	HRESULT hRes = S_OK;
 	m_bRecursive = (GetKeyState(VK_CONTROL)&0x80);
 	if (m_bRecursive) {
 		// Confirm action
@@ -173,36 +173,36 @@ STDMETHODIMP CLevelZapContextMenuExt::InvokeCommand(
 			return E_ABORT;
 	}
 
-    try {
-        if ((p_pCommandInfo == 0) || (p_pCommandInfo->cbSize < sizeof(CMINVOKECOMMANDINFO))) {
-            hRes = E_INVALIDARG;
-        } else {
-            // Get offset of invoked command.
-            UINT cmdOffset = (UINT) p_pCommandInfo->lpVerb;
-            if ((cmdOffset & 0xFFFF0000) != 0) {
-                // We do not support verb invokation.
-                hRes = E_FAIL;
-            } else if (!m_FirstCmdId.HasValue()) {
-                // We do not have menu items, so we can't invoke anything.
-                hRes = E_INVALIDARG;
-            } else {
-                // Check which command it is that is invoked.
-                UINT cmdId = cmdOffset + m_FirstCmdId;
-                if (m_ZapCmdId.HasValue() && m_ZapCmdId == cmdId) {
-                    // Zap everything.
-                    hRes = ZapAllFolders((p_pCommandInfo->fMask & CMIC_MASK_FLAG_NO_UI) == 0 ? p_pCommandInfo->hwnd : 0);
-                } else {
-                    // Invalid command ID.
-                    hRes = E_INVALIDARG;
-                }
-            }
-        }
-    } catch (...) {
-        hRes = E_UNEXPECTED;
-    }
+	try {
+		if ((p_pCommandInfo == 0) || (p_pCommandInfo->cbSize < sizeof(CMINVOKECOMMANDINFO))) {
+			hRes = E_INVALIDARG;
+		} else {
+			// Get offset of invoked command.
+			UINT cmdOffset = (UINT) p_pCommandInfo->lpVerb;
+			if ((cmdOffset & 0xFFFF0000) != 0) {
+				// We do not support verb invokation.
+				hRes = E_FAIL;
+			} else if (!m_FirstCmdId.HasValue()) {
+				// We do not have menu items, so we can't invoke anything.
+				hRes = E_INVALIDARG;
+			} else {
+				// Check which command it is that is invoked.
+				UINT cmdId = cmdOffset + m_FirstCmdId;
+				if (m_ZapCmdId.HasValue() && m_ZapCmdId == cmdId) {
+					// Zap everything.
+					hRes = ZapAllFolders((p_pCommandInfo->fMask & CMIC_MASK_FLAG_NO_UI) == 0 ? p_pCommandInfo->hwnd : 0);
+				} else {
+					// Invalid command ID.
+					hRes = E_INVALIDARG;
+				}
+			}
+		}
+	} catch (...) {
+		hRes = E_UNEXPECTED;
+	}
 
 	Util::OutputDebugStringEx(L"RETURN 0x%08x\n", hRes);
-    return hRes;
+	return hRes;
 }
 
 //
@@ -221,67 +221,67 @@ STDMETHODIMP CLevelZapContextMenuExt::InvokeCommand(
 // @return S_OK if successful, otherwise an error code.
 //
 STDMETHODIMP CLevelZapContextMenuExt::GetCommandString(
-    UINT_PTR p_CmdId,
-    UINT p_Flags,
-    UINT* p_pReserved,
-    LPSTR p_pBuffer,
-    UINT p_BufferSize)
+	UINT_PTR p_CmdId,
+	UINT p_Flags,
+	UINT* p_pReserved,
+	LPSTR p_pBuffer,
+	UINT p_BufferSize)
 {
-    HRESULT hRes = S_OK;
+	HRESULT hRes = S_OK;
 
-    try {
-        // Check what is requested.
-        if ((p_Flags == GCS_VERBA) || (p_Flags == GCS_VERBW)) {
-            // We do not support verb invokation.
-            hRes = E_NOTIMPL;
-        } else if ((p_Flags == GCS_VALIDATEA) || (p_Flags == GCS_VALIDATEW)) {
-            // We need to validate command ID.
-            if (m_FirstCmdId.HasValue() && m_ZapCmdId.HasValue() && m_ZapCmdId == (m_FirstCmdId + p_CmdId)) {
-                hRes = S_OK;
-            } else {
-                hRes = S_FALSE;
-            }
-        } else if (p_Flags == GCS_HELPTEXTA) {
-            // Call this method to get the Unicode version.
-            ArrayAutoPtr<wchar_t> wBuffer(new wchar_t[p_BufferSize]);
-            hRes = this->GetCommandString(p_CmdId,
-                                          GCS_HELPTEXTW,
-                                          p_pReserved,
-                                          reinterpret_cast<LPSTR>(wBuffer.Get()),
-                                          p_BufferSize);
-            if (SUCCEEDED(hRes)) {
-                // Convert it to a single-byte string and return it.
-                CStringA aBuffer(wBuffer.Get());
-                if (::strcpy_s(p_pBuffer, p_BufferSize, aBuffer) == 0) {
-                    hRes = S_OK;
-                } else {
-                    hRes = E_FAIL;
-                }
-            }
-        } else if (p_Flags == GCS_HELPTEXTW) {
-            // A Unicode help string is requested.
-            if (p_pBuffer != 0) {
-                if (m_FirstCmdId.HasValue() && m_ZapCmdId.HasValue() && m_ZapCmdId == (m_FirstCmdId + p_CmdId)) {
-                    // Return help text for our zap item.
-                    CStringW zapItemHint(MAKEINTRESOURCE(IDS_ZAP_MENU_ITEM_HINT));
-                    if (::wcscpy_s((LPWSTR) p_pBuffer, p_BufferSize, zapItemHint) != 0) {
-                        hRes = E_FAIL;
-                    }
-                } else {
-                    hRes = E_INVALIDARG;
-                }
-            } else {
-                hRes = E_INVALIDARG;
-            }
-        } else {
-            // Unknown, unsupported flag.
-            hRes = E_NOTIMPL;
-        }
-    } catch (...) {
-        hRes = E_UNEXPECTED;
-    }
+	try {
+		// Check what is requested.
+		if ((p_Flags == GCS_VERBA) || (p_Flags == GCS_VERBW)) {
+			// We do not support verb invokation.
+			hRes = E_NOTIMPL;
+		} else if ((p_Flags == GCS_VALIDATEA) || (p_Flags == GCS_VALIDATEW)) {
+			// We need to validate command ID.
+			if (m_FirstCmdId.HasValue() && m_ZapCmdId.HasValue() && m_ZapCmdId == (m_FirstCmdId + p_CmdId)) {
+				hRes = S_OK;
+			} else {
+				hRes = S_FALSE;
+			}
+		} else if (p_Flags == GCS_HELPTEXTA) {
+			// Call this method to get the Unicode version.
+			ArrayAutoPtr<wchar_t> wBuffer(new wchar_t[p_BufferSize]);
+			hRes = this->GetCommandString(p_CmdId,
+										  GCS_HELPTEXTW,
+										  p_pReserved,
+										  reinterpret_cast<LPSTR>(wBuffer.Get()),
+										  p_BufferSize);
+			if (SUCCEEDED(hRes)) {
+				// Convert it to a single-byte string and return it.
+				CStringA aBuffer(wBuffer.Get());
+				if (::strcpy_s(p_pBuffer, p_BufferSize, aBuffer) == 0) {
+					hRes = S_OK;
+				} else {
+					hRes = E_FAIL;
+				}
+			}
+		} else if (p_Flags == GCS_HELPTEXTW) {
+			// A Unicode help string is requested.
+			if (p_pBuffer != 0) {
+				if (m_FirstCmdId.HasValue() && m_ZapCmdId.HasValue() && m_ZapCmdId == (m_FirstCmdId + p_CmdId)) {
+					// Return help text for our zap item.
+					CStringW zapItemHint(MAKEINTRESOURCE(IDS_ZAP_MENU_ITEM_HINT));
+					if (::wcscpy_s((LPWSTR) p_pBuffer, p_BufferSize, zapItemHint) != 0) {
+						hRes = E_FAIL;
+					}
+				} else {
+					hRes = E_INVALIDARG;
+				}
+			} else {
+				hRes = E_INVALIDARG;
+			}
+		} else {
+			// Unknown, unsupported flag.
+			hRes = E_NOTIMPL;
+		}
+	} catch (...) {
+		hRes = E_UNEXPECTED;
+	}
 
-    return hRes;
+	return hRes;
 }
 
 //
@@ -296,14 +296,14 @@ STDMETHODIMP CLevelZapContextMenuExt::GetCommandString(
 //
 HRESULT CLevelZapContextMenuExt::ZapAllFolders(const HWND p_hParentWnd) const
 {
-    HRESULT hRes = S_OK;
-    bool yesToAll = !Util::QueryDWORDValueEx(L"PromptUser");
-    FolderV::const_iterator it, end = m_vFolders.end();
-    for (it = m_vFolders.begin(); it != end; ++it) {
+	HRESULT hRes = S_OK;
+	bool yesToAll = !Util::QueryDWORDValueEx(L"PromptUser");
+	FolderV::const_iterator it, end = m_vFolders.end();
+	for (it = m_vFolders.begin(); it != end; ++it) {
 		if (GetFileAttributes(*it)&FILE_ATTRIBUTE_DIRECTORY || m_bRecursive)
 			hRes = ZapFolder(p_hParentWnd, *it, yesToAll);
 	}
-    return hRes;
+	return hRes;
 }
 
 //
@@ -318,8 +318,8 @@ HRESULT CLevelZapContextMenuExt::ZapAllFolders(const HWND p_hParentWnd) const
 // @return Result code.
 //
 HRESULT CLevelZapContextMenuExt::ZapFolder(const HWND p_hParentWnd,
-                                           CString p_Folder,
-                                           bool& p_rYesToAll) const {
+										   CString p_Folder,
+										   bool& p_rYesToAll) const {
 	CString folderName = Util::PathFindFolderName(p_Folder);
 
 	// Ask for confirmation.
@@ -329,7 +329,7 @@ HRESULT CLevelZapContextMenuExt::ZapFolder(const HWND p_hParentWnd,
 	CString confirmMsgOld1(MAKEINTRESOURCE(IDS_ZAP_CONFIRM_MESSAGE_OLD_1));
 	CString confirmMsgOld2(MAKEINTRESOURCE(IDS_ZAP_CONFIRM_MESSAGE_OLD_2));
 	CString confirmMsgCompleteOld = confirmMsgOld1 + folderName + confirmMsgOld2;
-    if (!p_rYesToAll && !m_bRecursive)
+	if (!p_rYesToAll && !m_bRecursive)
 		if (!Dialog::doModal(p_hParentWnd, Util::GetVersionEx2()>=6?confirmMsgComplete.GetBuffer():confirmMsgCompleteOld.GetBuffer())) return E_ABORT;
 
 	// Check for name collission
@@ -356,8 +356,8 @@ HRESULT CLevelZapContextMenuExt::ZapFolder(const HWND p_hParentWnd,
 		DeleteFolder(p_hParentWnd, p_Folder);
 		return S_OK;
 	} else
-        return E_FAIL;
-    return S_OK;
+		return E_FAIL;
+	return S_OK;
 }
 
 //
